739259dd4e88105c9851235f5c2bef207dfc8157
jason zhao <yzhao3@stanford.edu>
2014-04-02 00:20:02 -0700

    Initial Commit

---
+++ VirtualPT/VPTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  VPTAppDelegate.h
+//  VirtualPT
+//
+//  Created by Jason Zhao on 4/2/14.
+//  Copyright (c) 2014 Jason Zhao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VPTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ VirtualPT/VPTViewController.h
@@ -0,0 +1,13 @@
+//
+//  VPTViewController.h
+//  VirtualPT
+//
+//  Created by Jason Zhao on 4/2/14.
+//  Copyright (c) 2014 Jason Zhao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VPTViewController : UIViewController
+
+@end
